9510d911f50e68f1882bf662cd322f0bfc2d00e1
Kuki031 <luka.kukucka1@gmail.com>
2024-06-06 19:09:27 +0200

    Add KnockKnockClient, add KnockKnockServer

---
+++ src/main/java/org/Luka/KnockKnockServer.java
@@ -0,0 +1,27 @@
+package org.Luka;
+
+import java.net.*;
+import java.io.*;
+
+public class KnockKnockServer {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) {
+            System.err.println("Usage: java KnockKnockServer <port number>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+
+        boolean listening = true;
+
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            while (listening) {
+                new KKMultiServerThread(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+}
