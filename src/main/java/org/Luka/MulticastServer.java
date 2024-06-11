ecbf4998dedbca6355969172cba4e4c59d797b4a
Kuki031 <luka.kukucka1@gmail.com>
2024-06-11 15:35:22 +0200

    Add MulticastClient, MulticastServer, MulticastServerThread and quotes file

---
+++ src/main/java/org/Luka/MulticastServer.java
@@ -0,0 +1,7 @@
+package org.Luka;
+import java.io.*;
+public class MulticastServer {
+    public static void main(String[] args) throws java.io.IOException {
+        new MulticastServerThread().start();
+    }
+}
