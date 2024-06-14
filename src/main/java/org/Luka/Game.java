0bbb18bb6eaf0e53a9d5f76294519d329748deb1
Kuki031 <luka.kukucka1@gmail.com>
2024-06-14 20:32:53 +0200

    Add Game class, convert Game to singleton

---
+++ src/main/java/org/Luka/Game.java
@@ -0,0 +1,115 @@
+package org.Luka;
+
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+import java.nio.*;
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+
+public class Game {
+
+    private static Game instance;
+
+    public static Game getInstance() {
+        if (instance == null) {
+            instance = new Game();
+        }
+        return instance;
+    }
+
+    private long window;
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+
+    private Rectangle rect1;
+    private Rectangle rect2;
+
+    public void run() {
+        init();
+        loop();
+
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    private void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if (!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+        // Create the window
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Java Game", NULL, NULL);
+        if (window == NULL)
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                glfwSetWindowShouldClose(window, true);
+        });
+
+        try (MemoryStack stack = stackPush()) {
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        }
+
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+        glfwShowWindow(window);
+
+        GL.createCapabilities();
+
+        rect1 = new Rectangle(-0.5f, 0.0f, 0.10f, 0.15f, 1.0f, 0.20f, 0.5f);
+        rect2 = new Rectangle(0.5f, 0.0f, 0.10f, 0.15f, 0.5f, 0.0f, 0.75f);
+    }
+
+    private void loop() {
+        Shader shaderProgram = Shader.getInstance();
+
+        glClearColor(0.0f, 0.5f, 1.0f, 1.0f);
+
+        while (!glfwWindowShouldClose(window)) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            rect1.render(shaderProgram);
+            rect2.render(shaderProgram);
+
+            rect1.handleKeyboardInput(window, "left");
+            rect2.handleKeyboardInput(window, "right");
+
+
+            if (rect1.isCollidingWith(rect2)) System.out.println("Collision detected!");
+
+
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    }
+
+    public static void main(String[] args) {
+        Game.getInstance().run();
+    }
+}
